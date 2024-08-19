950b0fc31b0862e5bbcba9814d26231c3fff1c2f
2320030141 <2320030141@klh.edu.in>
2024-08-19 22:41:56 +0530

    Delete client.java

--- client.java
+++
@@ -1,9 +0,0 @@
-public class SmartHomeClient {
-    public static void main(String[] args) {
-        SmartHomeFacade smartHome = new SmartHomeFacade();
-        
-        smartHome.activateMorningMode();
-        smartHome.leaveHome();
-        smartHome.activateNightMode();
-    }
-}
